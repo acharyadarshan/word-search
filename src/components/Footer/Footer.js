391f01da2259cf970fd6ee3d6624c1fda22bc866
Darshan Acharya <acharyadarshan02@gmail.com>
2022-03-15 14:58:29 +0545

    local app deployment

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <hr style={{ width: "90%", marginTop: 20 }} />
+      <span className="name">
+        Made by{" "}
+        
+          Darshan
+        
+      </span>
+      <div className="iconContainer">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
